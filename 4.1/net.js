e83cd1c4521aa14db5db2d2ad1092d9c06ee87ae
Anderson Yang <Anderson.Yang@sdcounty.ca.gov>
2019-11-26 15:35:33 -0800

    completed chat server

--- 4.1/net.js
+++ 4.1/net.js
@@ -1,18 +1,36 @@
 const server = require('net').createServer();
+let counter = 0;
+let sockets = {};
+
+function timestamp() {
+    const now = new Date();
+    return `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+}
 
 server.on('connection', socket => {
-  console.log('Client connected');
-  socket.write('Welcome new client!\n');
+    socket.id = counter++;
 
-  socket.on('data', data => {
-    console.log('data is:', data);
-    socket.write('data is: ');
-    socket.write(data);
-  });
+    console.log('Client connected');
+    socket.write('Please type your name:');
 
-  socket.on('end', () => {
-    console.log('Client disconnected');
-  });
+    socket.on('data', data => {
+        if (!sockets[socket.id]) {
+            socket.name = data.toString().trim();
+            socket.write(`Welcome ${socket.name}!\n`);
+            sockets[socket.id] = socket;
+            return;
+        }
+
+        Object.entries(sockets).forEach(([key, cs]) => {
+            if (socket.id == key) return;
+            cs.write(`${socket.name} ${timestamp()}: ${data}`);
+        });
+    });
+
+    socket.on('end', () => {
+        delete sockets[socket.id];
+        console.log(`${socket.name} disconnected`);
+    });
 });
 
 server.listen(8000, () => console.log('Server bound'));
