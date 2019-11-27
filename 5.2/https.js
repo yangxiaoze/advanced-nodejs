f056047c5b4ea41a249afc4e9ec312f226546605
Anderson Yang <Anderson.Yang@sdcounty.ca.gov>
2019-11-27 14:22:35 -0800

    added comments of creating certificate command

--- 5.2/https.js
+++ 5.2/https.js
@@ -1,13 +1,19 @@
+/**
+ * The command to generate a self-signed certificate
+ * 
+ * openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -nodes
+ */
+
 const fs = require('fs');
 const server = require('https')
-  .createServer({
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem'),
-  });
+    .createServer({
+        key: fs.readFileSync('./key.pem'),
+        cert: fs.readFileSync('./cert.pem'),
+    });
 
 server.on('request', (req, res) => {
-  res.writeHead(200, { 'content-type': 'text/plain' });
-  res.end('Hello world\n');
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.end('Hello world\n');
 });
 
 server.listen(443);
